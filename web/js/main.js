d2c61c312150b6c690b55d2154b85a51f3516c42
stevedavid <steve@steve-david.com>
2016-02-06 01:37:27 +0100

    fixed anchor probleme on URL changing

--- web/js/main.js
+++ web/js/main.js
@@ -65,7 +65,7 @@ $(document).ready(function ($) {
             $('html, body').stop().animate({
                 'scrollTop': $target.offset().top
             }, 500, 'swing', function () {
-                window.location.hash = target;
+                //window.location.hash = target;
                 $(document).on("scroll", onScroll);
             });
                 window.history.pushState({},"", href);
